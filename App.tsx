fec29340cbca8691d048cc318fa8c140c8d1168e
Ethan Iceberg <ethaniceberg@dagger.co.kr>
2024-01-04 09:44:45 +0900

    Feat: 프로젝트 생성
    HTML Editor 예제를 위한 프로젝트 생성

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>HTML Editor Example</Text>
+    </View>
+  );
+};
+
+export default App;
